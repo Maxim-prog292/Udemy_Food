2187f8169410076fc28af5e59cf3f640f970e9b9
Максим Магомедов <78023667+Maxim-prog292@users.noreply.github.com>
2021-12-13 20:37:04 +0300

    init: start UdemyFood
    Start project.
    Add tabs.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const parentTabs = document.querySelector('.tabheader__items'),
+          tabs = document.querySelectorAll('.tabheader__item'),
+          tabContent = document.querySelectorAll('.tabcontent');
+        
+    function hideTabContent() {
+        tabContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+        tabs.forEach(item => item.classList.remove('tabheader__item_active'));
+    }
+
+    function showTabContent(i = 0) {
+        tabContent[i].classList.add('show');
+        tabContent[i].classList.remove('hide');
+
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    parentTabs.addEventListener('click', e => {
+        const t = e.target;
+
+        if (t && t.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (t == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
